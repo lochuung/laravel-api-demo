c35783308ad844b7349fe1430851b0e300a51d93
Nguyen Huu Loc <locn562836@gmail.com>
2025-08-01 11:42:29 +0700

    feat: add inventory management features, including events, services, and repository

---
+++ app/Listeners/SendWelcomeEmail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\CustomerCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendWelcomeEmail
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(CustomerCreated $event): void
+    {
+        //
+    }
+}
